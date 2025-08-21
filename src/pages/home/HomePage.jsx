d3703cd47ec4f4e935ed30425cf357a16fc9caa0
Himanshu12866 <manshu010m@gmail.com>
2025-08-21 22:47:24 +0530

    Modified

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,7 +4,6 @@ const HomePage = () => {
   return (
     <>
       <title>Himanshu | MERN Stack & Frontend Developer Portfolio</title>
-      <h1 className="text-4xl font-bold top-0 -m-12">lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</h1>
     </>
   );
 };
