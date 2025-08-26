26c01f8fffb609f20df1d41e783f273ec744bd29
himanshu-novatales <himanshu.m@novatales.com>
2025-08-26 13:45:30 +0530

    feat: Update Footer component with contact information and improve layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,8 +6,8 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import XIcon from "@mui/icons-material/X";
 import { AppContext } from "../context/datacontext";
 import { Link } from "react-router-dom";
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const Footer = () => {
   const { darkMode } = useContext(AppContext);
@@ -29,18 +29,20 @@ const Footer = () => {
   return (
     <>
       <div
-        className={`flex justify-center    w-full  px-6 pt-12  ${!darkMode
-          ? "bg-[rgba(222,222,222,0.9)]"
-          : "bg-[rgba(69,69,69,1)] inset-0"
-          }`}
+        className={`flex justify-center w-full  px-6 pt-12  ${
+          !darkMode
+            ? "bg-[rgba(222,222,222,0.9)]"
+            : "bg-[rgba(69,69,69,1)] inset-0"
+        }`}
       >
         <div
           style={cardStyle}
-          className={`${!darkMode ? "bg-[#f5f5f52d]" : "bg-[#f5f5f533]"
-            }   shadow-[2px_4px_30px_rgba(0,0,0,0.1)] w-full  backdrop-blur-[5px] `}
+          className={`${
+            !darkMode ? "bg-[#f5f5f52d]" : "bg-[#f5f5f533]"
+          }   shadow-[2px_4px_30px_rgba(0,0,0,0.1)] w-full  backdrop-blur-[5px] `}
         >
-          <div className="w-full flex flex-wrap  justify-between p-12">
-            <div className=" w-1/4 flex flex-col ">
+          <div className="w-full grid xl:grid-cols-4  sm:grid-cols-2 md:gap-12 gap-2 xl:gap-0 justify-between 2xl:p-12 md:p-10 p-[40px_30px]">
+            <div className=" flex flex-col gap-2 lg:ps-12">
               <a href="/" className=" w-48">
                 <img
                   src={!darkMode ? dark_logo : light_logo}
@@ -48,25 +50,41 @@ const Footer = () => {
                   alt="Flowbite Logo"
                 />
               </a>
-              <h3 className="text-xl  font-medium font-heading pt-8 pb-2">
+              <h3 className="text-xl  font-medium font-heading pt-4 pb-2">
                 Himanshu Kumar Mishra
               </h3>
               <p className="text-xl font-para font-medium pe-8">
-                Passionate about building clean and modern UIs
+                Passionate about building clean and modern UIs & Web Apps
+              </p>
+              <p className="text-xl font-para font-medium pe-8">
+                <a
+                  href="mailto:manshu010m@gmail.com"
+                  className="underline hover:text-[#ff7700] transition-all hover:duration-200"
+                >
+                  manshu010m@gmail.com
+                </a>
+              </p>
+              <p className="text-xl font-para font-medium pe-8">
+                <a
+                  href="tel:+917804825835"
+                  className="underline hover:text-[#ff7700] transition-all hover:duration-200"
+                >
+                  +91 78048 25835
+                </a>
               </p>
             </div>
-            <div className="w-1/4 ps-12">
+            <div className="md:ps-12 ps-0">
               <h3 className="text-2xl pt-2">Experties</h3>
-              <ul className="flex flex-col font-medium p-4 md:p-0 capitalize mt-8 font-heading text-xl ">
+              <ul className="flex flex-col font-medium p-4 md:p-0 capitalize md:mt-8 font-heading text-xl ">
                 <li className="my-1">Frontend Developer</li>
                 <li className="my-1">React JS Developer</li>
                 <li className="my-1">UI Full Stack Developer</li>
                 <li className="my-1">MERN Stack Developer</li>
               </ul>
             </div>
-            <div className=" w-1/4 ps-12">
+            <div className="  lg:ps-12 ps-0">
               <h3 className="text-2xl pt-2">Links</h3>
-              <ul className="flex flex-col font-medium p-4 md:p-0 capitalize mt-8 font-heading text-xl ">
+              <ul className="flex flex-col font-medium p-4 md:p-0 capitalize md:mt-8 font-heading text-xl ">
                 {links.map((item, index) => (
                   <Link className="my-1" key={index} to={item.link}>
                     {item.name}
@@ -74,12 +92,12 @@ const Footer = () => {
                 ))}
               </ul>
             </div>
-            <div className="w-1/4">
+            <div className="xl:ps-0 md:ps-12 ps-0">
               <h3 className="text-2xl pt-2">Social</h3>
-              <ul className="flex flex-col font-medium p-4 md:p-0 capitalize mt-8 font-heading text-xl  ">
+              <ul className="flex flex-col font-medium p-4 md:p-0 capitalize md:mt-8 font-heading text-xl  ">
                 <li className="my-1">
                   <a
-                    className="flex flex-row gap-4 items-center"
+                    className="flex flex-row gap-4 items-center hover:text-[#ff7700] transition-all hover:duration-200"
                     href="https://github.com/himanshu12866"
                     target="_blank"
                     rel="noreferrer noopener"
@@ -90,8 +108,8 @@ const Footer = () => {
                 </li>{" "}
                 <li className="my-2">
                   <a
-                    className="flex flex-row gap-4 items-center"
-                    href="https://github.com/himanshu12866"
+                    className="flex flex-row gap-4 items-center hover:text-[#ff7700] transition-all hover:duration-200"
+                    href="https://linkedin.com/in/himanshumishra17/"
                     target="_blank"
                     rel="noreferrer noopener"
                   >
@@ -101,8 +119,8 @@ const Footer = () => {
                 </li>{" "}
                 <li className="my-2">
                   <a
-                    className="flex flex-row gap-4 items-center"
-                    href="https://github.com/himanshu12866"
+                    className="flex flex-row gap-4 items-center hover:text-[#ff7700] transition-all hover:duration-200"
+                    href="https://twitter.com/hema_nshu"
                     target="_blank"
                     rel="noreferrer noopener"
                   >
@@ -112,22 +130,29 @@ const Footer = () => {
                 </li>{" "}
               </ul>
               <form className="mt-4">
-                <label className="text-xl font-heading  font-medium pe-8">Let's connect!</label>
-                <input
-                  type="email" placeholder="Your Email"
-                  className={`bg-transparent py-3 mt-4 outline-none px-4 w-4/5 rounded-[10px] 
+                <label className="text-xl font-heading  font-medium pe-8">
+                  Let's connect!
+                </label>
+                <div className="flex lg:flex-row flex-col  lg:items-center lg:gap-4">
+                  <input
+                    type="email"
+                    placeholder="Your Email *"
+                    className={`bg-transparent py-3 mt-4 outline-none px-4 2xl:w-3/5 w-full rounded-[10px] 
 
-${darkMode
-                      ? "placeholder:text-white shadow-[0px_0.7066px_0.7066px_-0.6666px_rgba(0,0,0,0.08),0px_1.8065px_1.8065px_-1.3333px_rgba(0,0,0,0.08),0px_3.6217px_3.6217px_-2px_rgba(0,0,0,0.07),0px_6.8656px_6.8656px_-2.6666px_rgba(0,0,0,0.07),0px_13.6467px_13.6467px_-3.3333px_rgba(0,0,0,0.05),0px_30px_30px_-4px_rgba(0,0,0,0.02),inset_0px_3px_0px_0px_rgba(0,0,0,0.02)]"
-                      : "shadow-[0px_0.7066px_0.7066px_-0.6666px_rgba(0,0,0,0.08),0px_1.8065px_1.8065px_-1.3333px_rgba(0,0,0,0.08),0px_3.6217px_3.6217px_-2px_rgba(0,0,0,0.07),0px_6.8656px_6.8656px_-2.6666px_rgba(0,0,0,0.07),0px_13.6467px_13.6467px_-3.3333px_rgba(0,0,0,0.05),0px_30px_30px_-4px_rgba(0,0,0,0.02),inset_0px_3px_1px_0px_rgb(255,255,255)]"
-                    }
+${
+  darkMode
+    ? "placeholder:text-white shadow-[0px_0.7066px_0.7066px_-0.6666px_rgba(0,0,0,0.08),0px_1.8065px_1.8065px_-1.3333px_rgba(0,0,0,0.08),0px_3.6217px_3.6217px_-2px_rgba(0,0,0,0.07),0px_6.8656px_6.8656px_-2.6666px_rgba(0,0,0,0.07),0px_13.6467px_13.6467px_-3.3333px_rgba(0,0,0,0.05),0px_30px_30px_-4px_rgba(0,0,0,0.02),inset_0px_3px_0px_0px_rgba(0,0,0,0.02)]"
+    : "shadow-[0px_0.7066px_0.7066px_-0.6666px_rgba(0,0,0,0.08),0px_1.8065px_1.8065px_-1.3333px_rgba(0,0,0,0.08),0px_3.6217px_3.6217px_-2px_rgba(0,0,0,0.07),0px_6.8656px_6.8656px_-2.6666px_rgba(0,0,0,0.07),0px_13.6467px_13.6467px_-3.3333px_rgba(0,0,0,0.05),0px_30px_30px_-4px_rgba(0,0,0,0.02),inset_0px_3px_1px_0px_rgb(255,255,255)]"
+}
 
-                  `} />
-                  <div className=" flex justify-start pt-4 items-center w-4/5">
-                            <button className="bg-black font-para  text-white sm:px-8 px-4 py-3 font-medium rounded-[10px] opacity-100 hover:opacity-60 transition-all hover:duration-200 shadow-[rgba(61,61,61,0.72)_0px_0.602187px_1.08394px_-1.25px,rgba(61,61,61,0.64)_0px_2.28853px_4.11936px_-2.5px,rgba(61,61,61,0.25)_0px_10px_18px_-3.75px,rgba(0,0,0,0.35)_0px_0.706592px_0.706592px_-0.583333px,rgba(0,0,0,0.34)_0px_1.80656px_1.80656px_-1.16667px,rgba(0,0,0,0.33)_0px_3.62176px_3.62176px_-1.75px,rgba(0,0,0,0.3)_0px_6.8656px_6.8656px_-2.33333px,rgba(0,0,0,0.26)_0px_13.6468px_13.6468px_-2.91667px,rgba(0,0,0,0.15)_0px_30px_30px_-3.5px]">
-                                <a href="/about">Send</a>
-                            </button>
-                        </div>
+                  `}
+                  />
+                  <div className=" flex justify-start pt-4 items-center">
+                    <button className="bg-black font-para  text-white  px-4 py-3 font-medium rounded-[10px] opacity-100 hover:opacity-60 transition-all hover:duration-200 shadow-[rgba(61,61,61,0.72)_0px_0.602187px_1.08394px_-1.25px,rgba(61,61,61,0.64)_0px_2.28853px_4.11936px_-2.5px,rgba(61,61,61,0.25)_0px_10px_18px_-3.75px,rgba(0,0,0,0.35)_0px_0.706592px_0.706592px_-0.583333px,rgba(0,0,0,0.34)_0px_1.80656px_1.80656px_-1.16667px,rgba(0,0,0,0.33)_0px_3.62176px_3.62176px_-1.75px,rgba(0,0,0,0.3)_0px_6.8656px_6.8656px_-2.33333px,rgba(0,0,0,0.26)_0px_13.6468px_13.6468px_-2.91667px,rgba(0,0,0,0.15)_0px_30px_30px_-3.5px]">
+                      Send
+                    </button>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
