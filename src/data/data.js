f40ce798fb52c9196692e6d0fbb6659f0b64eb1d
Himanshu12866 <manshu010m@gmail.com>
2025-09-10 23:42:32 +0530

    feat: expand portfolioData structure with detailed sections for Home, About, and Projects pages

--- src/data/data.js
+++ src/data/data.js
@@ -1,2 +1,115 @@
-const portfolioData = []
+const portfolioData = [
+    {
+        id: 1,
+        title: "Home Page Data",
+        // Skills Data
+        data: [{
+            id: 1,
+            title: "Skills",
+            skills: {
+                name: "React JS",
+                iconsmall: "react",
+                iconlarge: "react",
+            }
+        },
+        //About Me Data Home Page
+        {
+            id: 2,
+            title: "About Me",
+            aboutme: {
+                company: "",
+                role: "",
+                duration: "",
+                description: "",
+                location:"",
+                locationlink:""
+                
+            }
+        },
+
+        // Projects in Home Page
+        {
+            id: 3,
+            title: "Projects",
+            projects: [
+                {
+                    projectId: 1,
+                    projectName: "",
+                    projectDesc: "",
+                    projectStacks: [],
+                    projectLink: "",
+                    projectCaseStudyLink: "",
+
+                },
+                {
+                    projectId: 1,
+                    projectName: "",
+                    projectDesc: "",
+                    projectStacks: [],
+                    projectLink: "",
+                    projectCaseStudyLink: "",
+                },
+            ]
+        },
+        ],
+    },
+    //Home Page Data completed
+
+    // About Page Data
+
+    {
+        id: 2,
+        title: "About Page Data",
+        data:[
+            {
+                profile:{
+                    pic:"",
+                    aboutmeparagraph:"",
+                    description:"",
+                    hobbies:[],
+                    strength:[],
+                },
+                experience:[
+                    {
+                        id:1,
+                        company:"",
+                        role:"",
+                        logo:"",
+                        location:"",
+                        duration:"",
+                        certificateLink:"",
+                        description:"",
+                        responsibilities:[],
+                         locationlink:""
+                    },
+                    {
+                        id:2,
+                        company:"",
+                        role:"",
+                        logo:"",
+                        location:"",
+                         locationlink:"",
+                        duration:"",
+                        certificateLink:"",
+                        description:"",
+                        responsibilities:[]
+                    },
+                ]
+                
+            }
+        ]
+
+    },
+    {
+        id: 3,
+        title: "Projects Page Data",
+        data:{
+            professinalprojects:[
+                {
+                    
+                }
+            ],
+        }
+    }
+]
 export default portfolioData
