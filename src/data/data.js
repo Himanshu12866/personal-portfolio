d7f8ca1c5b838ea61c091e91e89e2e4517682d05
himanshu-novatales <himanshu.m@novatales.com>
2025-09-11 10:55:54 +0530

    feat: restructure portfolioData to separate Skills, About Me, and Projects sections for improved clarity and organization

--- src/data/data.js
+++ src/data/data.js
@@ -1,56 +1,58 @@
 const portfolioData = [
+    // Skills Data and icons 
+    {
+        id: 0,
+        title: "Skills",
+        skills: {
+            name: "React JS",
+            iconsmall: "react",
+            iconlarge: "react",
+        }
+    },
     {
         id: 1,
         title: "Home Page Data",
-        // Skills Data
-        data: [{
-            id: 1,
-            title: "Skills",
-            skills: {
-                name: "React JS",
-                iconsmall: "react",
-                iconlarge: "react",
-            }
-        },
-        //About Me Data Home Page
-        {
-            id: 2,
-            title: "About Me",
-            aboutme: {
-                company: "",
-                role: "",
-                duration: "",
-                description: "",
-                location:"",
-                locationlink:""
-                
-            }
-        },
 
-        // Projects in Home Page
-        {
-            id: 3,
-            title: "Projects",
-            projects: [
-                {
-                    projectId: 1,
-                    projectName: "",
-                    projectDesc: "",
-                    projectStacks: [],
-                    projectLink: "",
-                    projectCaseStudyLink: "",
+        data: [
+            //About Me Data Home Page
+            {
+                id: 11,
+                title: "About Me",
+                aboutme: {
+                    company: "",
+                    role: "",
+                    duration: "",
+                    description: "",
+                    location: "",
+                    locationlink: ""
 
-                },
-                {
-                    projectId: 1,
-                    projectName: "",
-                    projectDesc: "",
-                    projectStacks: [],
-                    projectLink: "",
-                    projectCaseStudyLink: "",
-                },
-            ]
-        },
+                }
+            },
+
+            // Projects in Home Page
+            {
+                id: 12,
+                title: "Projects",
+                projects: [
+                    {
+                        projectId: 121,
+                        projectName: "",
+                        projectDesc: "",
+                        projectStacks: [],
+                        projectLink: "",
+                        projectCaseStudyLink: "",
+
+                    },
+                    {
+                        projectId: 122,
+                        projectName: "",
+                        projectDesc: "",
+                        projectStacks: [],
+                        projectLink: "",
+                        projectCaseStudyLink: "",
+                    },
+                ]
+            },
         ],
     },
     //Home Page Data completed
@@ -60,42 +62,42 @@ const portfolioData = [
     {
         id: 2,
         title: "About Page Data",
-        data:[
+        data: [
             {
-                profile:{
-                    pic:"",
-                    aboutmeparagraph:"",
-                    description:"",
-                    hobbies:[],
-                    strength:[],
+                profile: {
+                    pic: "",
+                    aboutmeparagraph: "",
+                    description: "",
+                    hobbies: [],
+                    strength: [],
                 },
-                experience:[
+                experience: [
                     {
-                        id:1,
-                        company:"",
-                        role:"",
-                        logo:"",
-                        location:"",
-                        duration:"",
-                        certificateLink:"",
-                        description:"",
-                        responsibilities:[],
-                         locationlink:""
+                        id: 1,
+                        company: "",
+                        role: "",
+                        logo: "",
+                        location: "",
+                        duration: "",
+                        certificateLink: "",
+                        description: "",
+                        responsibilities: [],
+                        locationlink: ""
                     },
                     {
-                        id:2,
-                        company:"",
-                        role:"",
-                        logo:"",
-                        location:"",
-                         locationlink:"",
-                        duration:"",
-                        certificateLink:"",
-                        description:"",
-                        responsibilities:[]
+                        id: 2,
+                        company: "",
+                        role: "",
+                        logo: "",
+                        location: "",
+                        locationlink: "",
+                        duration: "",
+                        certificateLink: "",
+                        description: "",
+                        responsibilities: []
                     },
                 ]
-                
+
             }
         ]
 
@@ -103,10 +105,10 @@ const portfolioData = [
     {
         id: 3,
         title: "Projects Page Data",
-        data:{
-            professinalprojects:[
+        data: {
+            professinalprojects: [
                 {
-                    
+
                 }
             ],
         }
