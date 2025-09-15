785218f9c85bf474e102930e30bc26a1a8e0cc36
himanshu-novatales <himanshu.m@novatales.com>
2025-09-15 13:24:42 +0530

    removed particles component

--- src/pages/projects/ProjectPage.jsx
+++ src/pages/projects/ProjectPage.jsx
@@ -1,16 +1,16 @@
 import ProjectBanner from "../../components/projects/ProjectBanner";
 import ProjectAbout from "../../components/projects/ProjectAbout";
 import ProjectsAll from "../../components/projects/ProjectsAll";
-import Particles from "../../components/projects/ProjectBG";
+// import Particles from "../../components/projects/ProjectBG";
 const ProjectPage = () => {
   return (
     <>
       <ProjectBanner />
-      <div className="w-full h-4/5">
+      {/* <div className="w-full h-4/5">
 
 
       <Particles />
-      </div>
+      </div> */}
       <ProjectAbout />
       <ProjectsAll />
     </>
