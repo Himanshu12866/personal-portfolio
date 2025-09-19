e0a31b0df01d58e8535f2555414b0fc6c887aa85
himanshu-novatales <himanshu.m@novatales.com>
2025-09-19 10:49:30 +0530

    `Refactored portfolio structure and project card components`

--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ContactBanner from "../../components/contact/ContactBanner";
-import ContactForm from "../../components/contact/ContactForm";
+// import ContactForm from "../../components/contact/ContactForm";
 import HomeContact from "../../components/home/HomeContact";
+import { useLenis } from "../../components/SmoothScroll";
 
 const ContactPage = () => {
+    const lenisRef = useLenis();
+    useEffect(() => {
+      lenisRef?.current?.scrollTo(0); // top of the page
+    }, []);
   return (
     <div>
       <ContactBanner />
--- src/pages/projects/ProjectPage.jsx
+++ src/pages/projects/ProjectPage.jsx
@@ -1,8 +1,14 @@
 import ProjectBanner from "../../components/projects/ProjectBanner";
 import ProjectAbout from "../../components/projects/ProjectAbout";
 import ProjectsAll from "../../components/projects/ProjectsAll";
+import { useLenis } from "../../components/SmoothScroll";
+import { useEffect } from "react";
 // import Particles from "../../components/projects/ProjectBG";
 const ProjectPage = () => {
+    const lenisRef = useLenis();
+    useEffect(() => {
+      lenisRef?.current?.scrollTo(0); // top of the page
+    }, []);
   return (
     <>
       <ProjectBanner />
