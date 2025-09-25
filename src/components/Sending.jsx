3fa5be2b204548558afa77d612a2be49acb49b6e
himanshu-novatales <himanshu.m@novatales.com>
2025-09-25 17:29:50 +0530

    Modified

---
+++ src/components/Sending.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+const SendingMessage = () => {
+  return (
+    <div>
+      <DotLottieReact
+      src="https://lottie.host/2de8d80a-daca-433e-9d22-23789748bf31/324KrrqDl1.lottie"
+      loop
+      autoplay
+    />
+    </div>
+  )
+}
+
+export default SendingMessage
--- src/pages/projects/ProjectPage.jsx
+++ src/pages/projects/ProjectPage.jsx
@@ -4,6 +4,7 @@ import ProjectsAll from "../../components/projects/ProjectsAll";
 import { useLenis } from "../../components/SmoothScroll";
 import { useEffect } from "react";
 // import Particles from "../../components/projects/ProjectBG";
+// import SendingMessage from "../../components/Sending"
 const ProjectPage = () => {
     const lenisRef = useLenis();
     useEffect(() => {
@@ -14,6 +15,7 @@ const ProjectPage = () => {
       <ProjectBanner />
       <ProjectAbout />
       <ProjectsAll />
+     
     </>
   );
 };
